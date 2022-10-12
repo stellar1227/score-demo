88c4e6ea547cb8e075416506cc9423d8cfa60388
mijin.lee <halfjin7@gmail.com>
2022-10-12 17:57:39 +0900

    vite config setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import { fileURLToPath, URL } from 'node:url';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [ //plugin settings
+    vue(), 
+    vuetify({ autoImport: true }),
+  ],
+  resolve: { //entries options
+    '@': fileURLToPath(new URL('./src', import.meta.url)),
+  },
+  server: { //server options : default localhost
+    port: '3000', 
+  },
 })
